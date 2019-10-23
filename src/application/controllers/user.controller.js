32cddbe631d31ecb9cd5da23b5c80bf437eb87ed
Franco Meriles <fran.meriles@gmail.com>
2019-10-23 18:55:05 -0300

    Add error handler

--- src/application/controllers/user.controller.js
+++ src/application/controllers/user.controller.js
@@ -3,8 +3,12 @@ const userService = require('../../domain/service/user.service');
 const userCtrl = {};
 
 userCtrl.getUsers = async (req, res) => {
-  const users = await userService.getUsers();
-  res.status(200).json(users);
+  try {
+    const users = await userService.getUsers();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ error: true, message: error });
+  }
 };
 
 module.exports = userCtrl;
--- src/infrastructure/service/userRequest.js
+++ src/infrastructure/service/userRequest.js
@@ -3,6 +3,6 @@ const fetch = require('node-fetch');
 const makeRequest = () => fetch(`${process.env.USER_SERVICE_URL}/users`)
   .then((response) => response.json())
   .then((data) => data)
-  .catch((err) => console.error(err));
+  .catch((err) => err);
 
 module.exports = makeRequest;
