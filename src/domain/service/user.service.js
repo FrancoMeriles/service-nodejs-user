ae253969912e5d739fd1afa25fd480bc52727827
Franco Meriles <fran.meriles@gmail.com>
2019-10-23 21:01:19 -0300

    add missing error handler

--- src/domain/service/user.service.js
+++ src/domain/service/user.service.js
@@ -4,14 +4,18 @@ const getAge = require('../util/getAge');
 
 class User {
   static async getUsers() {
-    const response = await userRequest();
-    // Filter by active
-    const usersFilter = response.users.filter((user) => user.is_active);
-    // Sort by lastname
-    const usersSort = usersFilter.sort((a, b) => a.lastname.localeCompare(b.lastname));
-    // Add age property
-    const users = usersSort.map((user) => ({ ...user, age: getAge(user.birthday) }));
-    return users;
+    try {
+      const response = await userRequest();
+      // Filter by active
+      const usersFilter = response.users.filter((user) => user.is_active);
+      // Sort by lastname
+      const usersSort = usersFilter.sort((a, b) => a.lastname.localeCompare(b.lastname));
+      // Add age property
+      const users = usersSort.map((user) => ({ ...user, age: getAge(user.birthday) }));
+      return users;
+    } catch (error) {
+      return error;
+    }
   }
 }
 
